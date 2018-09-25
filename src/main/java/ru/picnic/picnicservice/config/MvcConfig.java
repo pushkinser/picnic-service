8b195bf871772f5dc038b23b985418f79e0db1b2
pushkinser <pushkinsergey.voronezh@gmail.com>
2018-09-25 18:08:40 +0300

    Добавлен контроллер для главной страницы

--- src/main/java/ru/picnic/picnicservice/config/MvcConfig.java
+++ src/main/java/ru/picnic/picnicservice/config/MvcConfig.java
@@ -10,7 +10,6 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/admin").setViewName("admin");
     }
---
+++ src/main/java/ru/picnic/picnicservice/controller/HomeController.java
@@ -0,0 +1,20 @@
+package ru.picnic.picnicservice.controller;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("home")
+    public ModelAndView getHomePageWithWelcome(@NonNull Principal principal) {
+        ModelAndView homePage = new ModelAndView();
+        String name = (principal != null) ? principal.getName() : "guest";
+        homePage.addObject("userName", name);
+        return homePage;
+    }
+}
